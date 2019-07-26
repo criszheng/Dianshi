583be95d1bc6661cf4cea9f07bc9aec76c35b503
jasperyuan <jasperyuan@tencent.com>
2019-07-26 21:38:43 +0800

    增加cos对象存储

---
+++ Test.go
@@ -0,0 +1,24 @@
+package main
+
+//func main() {
+//	cosUtil := common.CosUtil{}
+//	c := cosUtil.CreateCosClient()
+//	// 查询bucket
+//	//s, _, err := c.Service.Get(context.Background())
+//	//if err != nil {
+//	//	panic(err)
+//	//}
+//	//
+//	//for _, b := range s.Buckets {
+//	//	fmt.Printf("%#v\n", b)
+//	//}
+//
+//	name := "test/objectPut.go"
+//	//上传对象
+//	data := "test"
+//	common.Put(c, name, data)
+//	//删除对象
+//	//common.Delete(c, name)
+//
+//	fmt.Println("https://dianshi-1251023989.cos.ap-chengdu.myqcloud.com/" + name);
+//}
---
+++ common/CosUtil.go
@@ -0,0 +1,58 @@
+package common
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/tencentyun/cos-go-sdk-v5"
+	"github.com/tencentyun/cos-go-sdk-v5/debug"
+)
+
+type CosUtil struct {
+	client *cos.Client
+}
+
+func (util CosUtil) CreateCosClient() *cos.Client {
+	u, _ := url.Parse("https://dianshi-1251023989.cos.ap-chengdu.myqcloud.com")
+	b := &cos.BaseURL{BucketURL: u}
+	c := cos.NewClient(b, &http.Client{
+		Transport: &cos.AuthorizationTransport{
+			SecretID:  "AKIDlSyKUWjnAMUWNBFw8962796sX4d9Kv65",
+			SecretKey: "veBbrx6KVVP8g2DICuSp00kUkutg2JPJ",
+			Transport: &debug.DebugRequestTransport{
+				RequestHeader: true,
+				// Notice when put a large file and set need the request body, might happend out of memory error.
+				RequestBody:    false,
+				ResponseHeader: true,
+				ResponseBody:   true,
+			},
+		},
+	})
+
+	return c
+}
+
+func Delete(c *cos.Client, name string) {
+	resp, err := c.Object.Delete(context.Background(), name)
+	fmt.Println(resp)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Put(c *cos.Client, name string, data string) {
+
+	// Case1 normal put object
+	f := strings.NewReader(data)
+
+	resp, err := c.Object.Put(context.Background(), name, f, nil)
+
+	fmt.Println(resp.Status)
+
+	if err != nil {
+		panic(err)
+	}
+}
